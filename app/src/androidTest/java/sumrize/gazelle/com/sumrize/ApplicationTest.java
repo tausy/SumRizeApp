3e3b3680fd192fe04f805f4b92c4cfe48a7a6017
tausy <write2tausy@gmail.com>
2014-09-14 01:57:57 +0530

    AS Project for application

---
+++ app/src/androidTest/java/sumrize/gazelle/com/sumrize/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sumrize.gazelle.com.sumrize;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
